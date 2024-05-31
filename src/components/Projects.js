c9f3bce74b7c8feb01c35304fea15ff93ef08a40
neelansh27 <neelanshsharma30@gmail.com>
2024-05-31 17:11:51 +0530

    Fix image links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,10 +5,7 @@ const Projects = () => {
       <h2 className="col-head">Projects</h2>
       <ul id="project-list">
         <li className="project-item">
-          <img
-            src="https://lh3.googleusercontent.com/pw/AP1GczPR5BuUK7MQcvc1BKLnmvzG9aLNAWKWddpMlU8faEXji6s0DYFOE86DCGIV5c9ImhQIsc8JQHV7hBSdFIIPM1Hq9GSGcoI-QmSAmGzT0qvxusA-msHg80taBSv7zeHGn-K2Rvq1UUbYoR2V4tpD0qhAPEoNIfeG4rGLZMjA0RgPBZmnKfgUF_NrAYg2Gq_EPTPZItMwMXxNuemduGl6pgYZ-kUFLkiLyW6Y2xjXqP71CIbjhpCivgQaqxwb27oj-urr2NTAGJJH4PtBUSbcev8XigMscbyAZVScGnt30OENk2hdvADY_Iy9WM44wZz7lWeE-1D_-nB7br1RAgytMFBebGnODunIqZSCicBHCLJOT6ByxMLR0UtPAEadB-tyVSSgb5t19J0A4NFfZU2Mi_j1BCoLl6w5FzW54QiXCcqCtkisVtVyazJ_uQaFbrFtXtVMZuIz4v3QkBaURUr3kZn8egfzqkErmq9HgW8vxnUH5SgFnAKLC_YiPaHIP0RnHCsad2mdQFbunRj6StgHw94p53vFLOyykOZCzsUnaQWvGHGHtzxhlmpifQR9GQh4hU0RCCgCZtp8K9XfIsIOhPcUfniBuA5XT_vwGXLahqSWNNPYHLKGNfHzM5fSXwi_8dl4GgqA61sn2A_hHdK67nGIcGvHK39zjueE4O6S8DRqUqh_UuMPnEeOa0z2mivtxdo76XOeW5EivNrjMO8dUcVO0tvVXygDEe6xaWSva3iLOEzUoDomeFdra7gGLomKWHqaK-tAP_P1uCbVFl1qxt8zvlZt47XJ_OUj4HzMzjhww5gl1NJ4lT3netfEEAJIBMpNSUhkCAqF6Dw-bRXKAS910Yp-oI2swfpQKH-h3z9LgrBwpzbxAr-g5Kj0WGX1_HAmZrJVzfXwQJFXwJ6k3Way4q-fYQ62qwJ8rjFbUFELUmWxx70A6hybq6WVEwX7YyLNk7NlTFZRfetieSxVfAlDQ6Ks7wnQ2yXSH5X_fBYDbqN4FsxpT8vlPlNHdanSNA=w1036-h759-s-no-gm?authuser=0"
-            alt="temp"
-          />
+        <img src="https://i.ibb.co/zVKTPdf/Copy-Q-Hr-HMij.png" alt="temp" />
           <div>
             <h4>
               <a href="https://blogpress-client.vercel.app/" target={"_blank"}>
@@ -30,24 +27,24 @@ const Projects = () => {
           />
           <div>
             <h4>
-              <a href="https://legalnavigator-m4aw.onrender.com" target={"_blank"}>
+              <a
+                href="https://legalnavigator-m4aw.onrender.com"
+                target={"_blank"}
+              >
                 LegalNavigator
               </a>
             </h4>
             <span>
               A platform which allows people to stay legally informed. It has
-              features like check current court cases, summarize legal
-              texts, connect with experts and more. It is built
-              using HTML, CSS, JavaScript & Flask.&lrm;
+              features like check current court cases, summarize legal texts,
+              connect with experts and more. It is built using HTML, CSS,
+              JavaScript & Flask.&lrm;
             </span>
           </div>
         </li>
         <li className="project-item">
-          <img
-            src="https://lh3.googleusercontent.com/pw/AP1GczNKCxTzMFM8IS2p-o5IRuh64OK_wZBiBpIsCXxoYThyf0A40DewU0NX7tnql4zf4AibXhfIxSQ7cyIgA6qwzKYwArdTSJNS_oTIQuV9LKrxJi0-C5cFjlev187wvQnTDrfVIEVV5nPcboJDEUoRs3aEG3Feh6ApqwVv83Jrhnq7DBM7iGl_VSW36F5YZdQHN6v0l-hsaaFW8iAet8QYR5X51MQo2p_NiOdweHu-bAVU8hqPRGeQMDbvUwhKw1ePH4jXFYCZVwtlWsRBzHlMZb5iSVAykisAHysc3g5MnlyWQheVDXpABWQqVJR_wgUHPVmMiD9z0c2gMrWfSL-1-eJnFPH4rO6ypiURZUjgF78KNN5Y97gabdX92PGMjtoP4E4AVYATw1H6GUkVdeIc8XcgepxP4wzvPYn3q8UH8ppWfi-xCBptuJx97uJC3-WR5Vg-z3MaekJh3FxSvf8DqQZmjwJDFnarPxn72yHPtWQZE3tDsoSH09XmvC8C9Dr2X2_RTjROj8OuHP21glFnarbheg5wYlvbKVD7rcg1AQnOCzz-l3HmihKaOtnbLBgW-U2ysMDKR8w-Gn5Biug4GPfnD40C8SUPT5kQn3DBpq8tlrF1HkYWxKNmxQ3bRVbp8pzsIrKq5A0d3rsXIKV5c1MVa3nRKT8rmjYMs55xvWMou1zV3bvK-pTBYhFvE4QbQ-v6m1Yl0jBNfyAU1UWRGvMP4uulET8JM-wZ6InQdwULP9DcBjPZ2oOFNyfA5sfRRmSB0lALx46E9Vlf5QDPoGfgv2EoZjAiI9pB7Hv2Gxx78AfQdcmfB-SKxi_IOLwJv_aXlVAKBnrkEJt1LOjPiyJZhEeoFJmSp5EfPw-LqV72cp4OcX8We5xmdP0EY3wxag6HDECjXKtF-3ivZAmKyLBXPv-4s3v-sopIsDxKJz87RHeIqjK_ThYfh2JbCnBJ3lXIzjfwjITGCuZiTMG4DhbV_l4C6xGa5NDvPY4v2CVfPCFDh87bcrDhryvsZD_B=w664-h463-s-no-gm?authuser=0"
-            alt="temp"
-          />
-          <div>
+    <img src="https://i.ibb.co/bK3Sd2L/image.png" alt="image" />
+    <div>
             <h4>
               <a
                 href="https://github.com/neelansh27/Domestic-Aid-Frontend"
