0363625198a13099223e5bea5365d14035789e32
neelansh27 <neelanshsharma30@gmail.com>
2024-05-31 12:12:41 +0530

    Remove routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
 import './App.css'
 import Home from "./components/Home";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -12,11 +7,7 @@ import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
 library.add(fab, faCheckSquare, faCoffee,fas);
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={ <Home/> }/>
-    </Routes>
-    </BrowserRouter>
+    <Home/>
   );
 }
 
